70dd826ca80337d86326661954f2ec55a26aee19
anushkaaa34 <prathaagrawal97@gmail.com>
2024-07-11 02:23:42 +0530

    Revert "Merge pull request #4 from aryapandeyy22/anushkaaa34-patch-8"
    This reverts commit 473b57bc7f8cd7004759e61a598ce0c984b93730, reversing
    changes made to 69c4235edd003d2aa2e6e13a75637d07f5131c4f.

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/check-phone', authController.checkPhone);
-router.post('/send-otp', authController.sendOtp);
-router.post('/verify-otp', authController.verifyOtp);
-
-module.exports = router;
